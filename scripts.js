be89bbd9a57711eb767ff3126696134699c986c4
Allen Kinite <106151383+LNKNITE@users.noreply.github.com>
2022-05-26 23:34:31 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ function startTime() {
   let s = today.getSeconds();
   m = checkTime(m);
   s = checkTime(s);
-  document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
+  document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
   setTimeout(startTime, 1000);
 }
 
